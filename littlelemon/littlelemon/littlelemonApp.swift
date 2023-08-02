ffd03357e34576c6b246b0ecfeb5343330bc7d97
itsfloppyhat <itsfloppyhat@gmail.com>
2023-08-01 20:34:11 -0400

    initialized project

---
+++ littlelemon/littlelemon/littlelemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  littlelemonApp.swift
+//  littlelemon
+//
+//  Created by Matthew Lucas on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct littlelemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
